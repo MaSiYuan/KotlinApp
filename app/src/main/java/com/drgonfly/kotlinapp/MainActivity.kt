e8738992a3178056cf2126505f3262ac61e4690b
masy0502 <masy0502@thundersoft.com>
2019-01-11 16:28:05 +0800

    Update image text view

--- app/src/main/java/com/drgonfly/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/drgonfly/kotlinapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.drgonfly.kotlinapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import com.drgonfly.kotlinapp.view.state.StateLayout
+import com.drgonfly.kotlinapp.view.text.ImgTextView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), StateLayout.OnStateViewChildClickedListener,StateLayout.OnStateViewClickedListener {
+class MainActivity : AppCompatActivity(), StateLayout.OnStateViewChildClickedListener, StateLayout.OnStateViewClickedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +28,13 @@ class MainActivity : AppCompatActivity(), StateLayout.OnStateViewChildClickedLis
 
         state_layout.setOnStateViewClickedListener(this)
 
+        img_text.setOnClickListener { img_text.showText("老哥，遇到这样的公司我的建议是果断离职吧，或者找项目经理来协调这件事情，比如我们公司的迭代周期是两周，在需求讨论会之后大家需要把各端的排期定好，比如设计排期，接口排期，然后移动端制定移动端排期，接口不合理就得根据移动端业务场景修改，个人协调不好找移动端负责人和后端负责人协调沟通。即使有了这些保证我们上线也是会遇到因为接口问题上线推迟到凌晨，整的我都有点怀疑人生了，所以我才想着提升自己进大公司，把跟他们扯皮的时间用来学习上，所以总结下来找一个好的团队才是解决这个问题的关键。") }
+
+        img_text.addImgEngine(object : ImgTextView.ImgEngine {
+            override fun showImg(iv: ImageView) {
+                iv.setImageResource(R.drawable.ic_launcher_foreground)
+            }
+        })
     }
 
     override fun onChildClicked(childId: Int, view: View?) {
--- app/src/main/java/com/drgonfly/kotlinapp/view/text/ImgTextView.kt
+++ app/src/main/java/com/drgonfly/kotlinapp/view/text/ImgTextView.kt
@@ -1,11 +1,11 @@
 package com.drgonfly.kotlinapp.view.text
 
 import android.content.Context
-import android.graphics.Bitmap
+import android.content.res.TypedArray
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.Log
 import android.view.Gravity
 import android.widget.FrameLayout
 import android.widget.ImageView
@@ -14,49 +14,43 @@ import com.drgonfly.kotlinapp.util.Utils
 
 class ImgTextView(ctx: Context, attr: AttributeSet) : FrameLayout(ctx, attr) {
 
-    val TAG = ImgTextView::class.java.simpleName
-
-    val MARGIN = Utils.dp2px(20f)
-
-    val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-
-    var bitmap: Bitmap? = null
-    var text: String = "尽管Activity是系统提供的您的应用界面的入口点，但在相互分享数据以及转场方面，Activity表现得不够灵活，这就让它不适合作为构建您的应用内导航的理想架构。今天，我们宣布推出导航组件，作为构建您的应用内界面的框架，重点是让单 Activity 应用成为首选架构。利用导航组件对 Fragment 的原生支持，您可以获得架构组件的所有好处（例如生命周期和 ViewModel），同时让此组件为您处理 FragmentTransaction 的复杂性。此外，导航组件还可以让您声明我们为您处理的转场。它可以自动构建正确的“向上”和“返回”行为，包含对深层链接的完整支持，并提供了帮助程序，用于将导航关联到合适的 UI 小部件，例如抽屉式导航栏和底部导航。但这些并不是全部！"
-    var textSize: Float = 80f
-
-    var imgWidth = 300
-    var imgHeight = 300
-
-    val fontMetrics: Paint.FontMetrics = Paint.FontMetrics()
-
-    var measuredWidth = FloatArray(1)
-
-    init {
-        val iv = ImageView(ctx)
-        val lp: FrameLayout.LayoutParams = FrameLayout.LayoutParams(imgWidth, imgHeight, Gravity.TOP)
-        lp.topMargin = MARGIN.toInt()
-        lp.leftMargin = MARGIN.toInt()
-        iv.layoutParams = lp
-        addView(iv)
-
-        iv.setImageResource(R.drawable.ic_launcher_background)
-
-        paint.textSize = textSize
-        paint.getFontMetrics(fontMetrics)
-
-        Log.i(TAG, "fontMetrics.top = " + fontMetrics.top)
-        Log.i(TAG, "fontMetrics.bottom = " + fontMetrics.bottom)
-        Log.i(TAG, "fontMetrics.ascent = " + fontMetrics.ascent)
-        Log.i(TAG, "fontMetrics.descent = " + fontMetrics.descent)
-        Log.i(TAG, "fontMetrics.leading = " + fontMetrics.leading)
+    companion object {
+        val TAG: String = ImgTextView::class.java.simpleName
     }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
+    private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val fontMetrics: Paint.FontMetrics = Paint.FontMetrics()
+    private val measuredWidth = FloatArray(1)
+    private val imgView: ImageView = ImageView(ctx)
+
+    var paddingWithText:Float = 0f
+    var text: String = "Here is the content."
+
+    var imgWidth:Float = 0f
+    var imgHeight:Float = 0f
+
+    private var imgEngine: ImgEngine? = null
+
+    init {
+        val array: TypedArray = context.theme.obtainStyledAttributes(attr, R.styleable.ImgTextView, 0, 0)
+        imgWidth = array.getDimension(R.styleable.ImgTextView_img_width, 0f)
+        imgHeight = array.getDimension(R.styleable.ImgTextView_img_height, 0f)
+        paddingWithText = array.getDimension(R.styleable.ImgTextView_padding_with_text, 0f)
+        val textSize = array.getDimension(R.styleable.ImgTextView_text_size, 0f)
+        val textColor = array.getColor(R.styleable.ImgTextView_text_color, Color.DKGRAY)
+        array.recycle()
+
+        imgView.layoutParams = FrameLayout.LayoutParams(imgWidth.toInt(), imgHeight.toInt(), Gravity.TOP)
+
+        imgView.setBackgroundColor(Color.LTGRAY)
+        addView(imgView)
+
+        paint.textSize = textSize
+        paint.color = textColor
+        paint.getFontMetrics(fontMetrics)
     }
 
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
 
         var startIndex = 0
         val textLength = text.length
@@ -64,26 +58,50 @@ class ImgTextView(ctx: Context, attr: AttributeSet) : FrameLayout(ctx, attr) {
         var drawWidth: Float
 
         var x: Float
-        var y = -fontMetrics.ascent + MARGIN
+
+        var bottom = -fontMetrics.top + paddingTop
+        var top = paddingTop
 
         while (startIndex < textLength) {
-            if (y > MARGIN && y + fontMetrics.ascent < imgHeight + MARGIN) {
-                x = imgWidth.toFloat() + MARGIN
-                drawWidth = width.toFloat() - MARGIN * 2 - imgWidth
+            if (bottom > paddingTop && top < imgHeight + paddingTop) {
+                x = imgWidth + paddingLeft + paddingWithText
+                drawWidth = width.toFloat() - paddingLeft - paddingRight - paddingWithText - imgWidth
             } else {
-                x = MARGIN
-                drawWidth = width.toFloat() - MARGIN * 2
+                x = paddingLeft.toFloat()
+                drawWidth = width.toFloat() - paddingLeft - paddingRight
             }
 
-            val textCount = paint.breakText(text, true, drawWidth, measuredWidth)
-            val endIndex = Math.min(startIndex + textCount, textLength)
-
-            Log.e(TAG, "startIndex = $startIndex endIndex = $endIndex textCount = $textCount drawWidth = $drawWidth")
-
-            canvas?.drawText(text, startIndex, endIndex, x, y, paint)
+            val textCount = paint.breakText(text, startIndex, textLength, true, drawWidth, measuredWidth)
+            if (height - bottom < 1.5 * paint.fontSpacing) {
+                val lastLineText: String = if (textCount > 3) {
+                    text.substring(startIndex, startIndex + textCount - 3) + "..."
+                } else {
+                    text.substring(startIndex) + "..."
+                }
+                canvas?.drawText(lastLineText, x, bottom, paint)
+                break
+            } else {
+                canvas?.drawText(text, startIndex, startIndex + textCount, x, bottom, paint)
+            }
 
             startIndex += textCount
-            y += paint.fontSpacing
+
+            bottom += paint.fontSpacing
+            top = (bottom + fontMetrics.top).toInt()
         }
+    }
+
+    fun showText(text: String) {
+        this.text = text
+        invalidate()
+    }
+
+    fun addImgEngine(imgEngine: ImgEngine) {
+        this.imgEngine = imgEngine
+        imgEngine.showImg(imgView)
+    }
+
+    interface ImgEngine {
+        fun showImg(iv: ImageView)
     }
 }
