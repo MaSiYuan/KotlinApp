438ab5e53fd79eb22841d0ce222a50040fc4317b
MaSiYuan <msy543@126.com>
2019-10-30 22:00:54 +0800

    Update code

--- app/src/main/java/com/drgonfly/kotlinapp/view/image/CircleImage.kt
+++ app/src/main/java/com/drgonfly/kotlinapp/view/image/CircleImage.kt
@@ -9,45 +9,45 @@ import android.util.AttributeSet
 
 class CircleImage(ctx: Context, attr: AttributeSet) : AppCompatImageView(ctx, attr) {
 
-    val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    val xfermode: Xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
-    val savedArea: RectF = RectF()
+	val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+	val xfermode: Xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+	val savedArea: RectF = RectF()
 
-    var centerX = 0f
-    var centerY = 0f
-    var radius = 0f
+	var centerX = 0f
+	var centerY = 0f
+	var radius = 0f
 
-    var bitmap: Bitmap? = null
+	var bitmap: Bitmap? = null
 
-    init {
+	init {
 
-    }
+	}
 
-    override fun setImageBitmap(bm: Bitmap?) {
+	override fun setImageBitmap(bm: Bitmap?) {
 //        super.setImageBitmap(bm)
-        this.bitmap = bm
-    }
+		this.bitmap = bm
+	}
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
+	override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+		super.onSizeChanged(w, h, oldw, oldh)
 
-        centerX = (width / 2).toFloat()
-        centerY = (height / 2).toFloat()
-        radius = (width / 2).toFloat()
+		centerX = (width / 2).toFloat()
+		centerY = (height / 2).toFloat()
+		radius = (width / 2).toFloat()
 
-        savedArea.set(0f, 0f, width.toFloat(), height.toFloat())
-    }
+		savedArea.set(0f, 0f, width.toFloat(), height.toFloat())
+	}
 
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
+	@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+	override fun onDraw(canvas: Canvas?) {
+		super.onDraw(canvas)
 
-        canvas?.drawCircle(centerX, centerY, radius, paint)
-        val saved: Int? = canvas?.saveLayer(savedArea, paint)
-        canvas?.drawCircle(centerX, centerY, radius-10, paint)
-        paint.xfermode = xfermode
-        canvas?.drawBitmap(bitmap!!, 0f, 0f, paint)
-        paint.xfermode = null
-        canvas?.restoreToCount(saved!!)
-    }
+		canvas?.drawCircle(centerX, centerY, radius, paint)
+		val saved: Int? = canvas?.saveLayer(savedArea, paint)
+		canvas?.drawCircle(centerX, centerY, radius - 10, paint)
+		paint.xfermode = xfermode
+		canvas?.drawBitmap(bitmap!!, 0f, 0f, paint)
+		paint.xfermode = null
+		canvas?.restoreToCount(saved!!)
+	}
 }
